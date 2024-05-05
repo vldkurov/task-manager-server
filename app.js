65f5aea01c8a77394f56ac02bca37a929a804d17
Volodymyr Kurov <vokur13@gmail.com>
2024-05-05 14:23:11 +0100

    Add CORS support and update login response
    In this commit, the "cors" dependency is added to both package.json and package-lock.json to handle Cross-Origin Resource Sharing (CORS) concerns. The app.js file has been updated with a CORS configuration and the required middleware usage. Also, the server response for successful user login in login.js has been enhanced to return both user ID and username along with the auth token.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,6 +12,33 @@ const sequelize = require("./config/database");
 
 const app = express();
 
+const whitelist = [
+    // 'https://thunderous-moxie-f4ffbe.netlify.app', 'https://main--thunderous-moxie-f4ffbe.netlify.app', 'https://my-ecommerce-client.vercel.app',
+    'http://localhost:3000'
+];
+
+
+// CORS configuration
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("Origin of request " + origin);
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin permissible");
+            callback(null, true);
+        } else {
+            console.log("Origin blocked by CORS");
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    allowedHeaders: [
+        'Access-Control-Allow-Origin',
+        'Content-Type',
+        'Authorization',
+    ],
+};
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
             return res.status(401).json({error: 'Invalid password'});
         }
         const token = jwt.sign({id: user.id}, config.jwt_secret, {expiresIn: '1d'});
-        res.status(200).json({message: 'Login successful', token});
+        res.status(200).json({message: 'Login successful', user: {id: user.id, username: user.username}, token});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
